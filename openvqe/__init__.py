91e37570204dfce4c8fa91cdf34ead5d0b6c6009
Jakob S Kottmann <jakob.kottmann@gmail.com>
2019-06-03 14:47:08 -0400

    added imports to __init__.py

---
+++ openvqe/__init__.py
@@ -0,0 +1,3 @@
+from openvqe.exceptions import OpenVQEException, ParameterError
+from openvqe.parameters import ParametersHamiltonian, ParametersQC, ParametersPsi4
+from openvqe.hamiltonian.hamiltonian_qc import HamiltonianQC
