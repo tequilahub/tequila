c5c8b369ab66e28522121b697fa37ec67640ed60
Jakob S Kottmann <jakob.kottmann@gmail.com>
2019-08-06 15:12:26 -0400

    framework for gradients 2

--- examples/gradient.py
+++ examples/gradient.py
@@ -1,4 +1,4 @@
-from openvqe.circuit.circuit import QCircuit, Ry, X
+from openvqe.circuit.circuit import QCircuit, Ry, X, Rx, Rz
 from openvqe.observable import Observable, make_gradient
 from numpy import pi
 
@@ -20,3 +20,17 @@ if __name__ == "__main__":
     observable = Observable()
     observable.unitary=[ac]
     gradient = make_gradient(observable=observable)
+
+    print("gradient=", gradient)
+
+    ac = QCircuit()
+    ac += X(0)
+    ac += Rx(target=2, angle=0.5)
+    ac += Ry(target=1, control=0, angle=pi / 2)
+    ac += Rz(target=1, control=[0,2], angle=pi / 2)
+
+    observable = Observable()
+    observable.unitary=[ac]
+    gradient = make_gradient(observable=observable)
+
+    print("gradient=", gradient)
