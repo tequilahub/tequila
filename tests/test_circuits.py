7a9fe174340cfc4e7f050be128054dd6eb8ceffc
Jakob S Kottmann <jakob.kottmann@gmail.com>
2019-08-29 12:27:49 -0400

    more tests

--- tests/test_circuits.py
+++ tests/test_circuits.py
@@ -4,9 +4,9 @@ from openvqe.circuit.gates import wrap_gate
 from openvqe.circuit._gates_impl import RotationGateImpl
 import numpy
 
-def test_conventions():
 
-    qubit = numpy.random.randint(0,3)
+def test_conventions():
+    qubit = numpy.random.randint(0, 3)
     angle = "angle"
 
     Rx1 = Rx(target=qubit, angle=angle)
@@ -15,12 +15,24 @@ def test_conventions():
     Rx4 = RotationGate(axis=0, target=qubit, angle=angle)
     Rx5 = RotationGate(axis="X", target=qubit, angle=angle)
     Rx6 = RotationGate(axis="x", target=qubit, angle=angle)
+    Rx7 = RotationGate(axis=0, target=qubit, angle=angle)
+    Rx7.axis = "X"
 
-    ll = [Rx1, Rx2, Rx3, Rx4, Rx5, Rx6]
+    ll = [Rx1, Rx2, Rx3, Rx4, Rx5, Rx6, Rx7]
     for l1 in ll:
         for l2 in ll:
-            assert(l1==l2)
+            assert (l1 == l2)
 
+    qubit = 2
+    for c in [None, 0, 3]:
+        for angle in ["angle", 0, 1.234]:
+            for axes in [[0, "x", "X"], [1, "y", "Y"], [2, "z", "Z"]]:
+                ll = [RotationGate(axis=i, target=qubit, control=c, angle=angle) for i in axes]
+                for l1 in ll:
+                    for l2 in ll:
+                        assert (l1 == l2)
+                        l1.axis=axes[numpy.random.randint(0, 2)]
+                        assert (l1 == l2)
 
 
 def test_basic_gates():
@@ -63,7 +75,7 @@ def test_consistency():
 
 
 def test_arithmetic():
-    for c in [None, 4, [4,5]]:
+    for c in [None, 4, [4, 5]]:
         qubit = numpy.random.randint(0, 3)
         power = numpy.random.uniform(0, 5)
         X2 = X(target=qubit, control=c, power=power)
