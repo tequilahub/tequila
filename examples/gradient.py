b597b1c0b96d6e1153c2200886cff458b6c3eb26
Jakob S Kottmann <jakob.kottmann@gmail.com>
2019-08-29 11:08:48 -0400

    demonstrate gradient fail for controlled rotations

--- examples/gradient.py
+++ examples/gradient.py
@@ -82,16 +82,24 @@ if __name__ == "__main__":
     H = MyQubitHamiltonian()
     H.parameters.transformation = "my_trafo"
 
+
+    print("\n\nGradients of non-controlled gates work:\n")
+
     energies = []
     gradients = []
     angles = []
     for n in range(9):
         theta = n*pi/4
         U = Ry(target=0, angle=theta)
+        print(U)
+        result = SimulatorCirq().simulate_wavefunction(abstract_circuit=U)
+        print(result)
+        print(SimulatorCirq().create_circuit(abstract_circuit=U))
         O = Objective(observable=H, unitaries=U)
         E = SimulatorCirq().expectation_value(objective=O)
         energies.append(E)
         dE = SimulatorCirq().expectation_value(objective=grad(O)[0])
+        print(grad(O)[0])
         gradients.append(dE)
         angles.append(theta)
 
@@ -101,6 +109,70 @@ if __name__ == "__main__":
 
     from matplotlib import pyplot as plt
     plt.title("Gradient Test")
+    plt.xlabel("t")
+    plt.plot(angles, energies, label="E")
+    plt.plot(angles, gradients, label="dE/dt")
+    plt.legend()
+    plt.show()
+
+    print("\n\nDemonstrating faulty behaviour here:")
+    energies = []
+    gradients = []
+    angles = []
+    for n in range(9):
+        theta = n*pi/4
+        # multiplication syntax still follows circuits ... should probably change that at some point
+        U = X(target=1)*Ry(target=0, control=1, angle=theta)
+        # if you wanna see the states
+        wfn = SimulatorCirq().simulate_wavefunction(abstract_circuit=U)
+        # leaving this here to demonstrate how to use cirqs ascii print since we don't have our own right now
+        print(SimulatorCirq().create_circuit(abstract_circuit=U))
+        O = Objective(observable=H, unitaries=U)
+        E = SimulatorCirq().expectation_value(objective=O)
+        energies.append(E)
+        dE = SimulatorCirq().expectation_value(objective=grad(O)[0])
+        gradients.append(dE)
+        angles.append(theta)
+
+    print("Gradient fails!")
+    for i,a in enumerate(angles):
+        print(angles[i], " ", energies[i], " ", gradients[i])
+
+    from matplotlib import pyplot as plt
+    plt.title("Gradient Test for controlled Rotation")
+    plt.xlabel("t")
+    plt.plot(angles, energies, label="E")
+    plt.plot(angles, gradients, label="dE/dt")
+    plt.legend()
+    plt.show()
+
+
+    print("\n\nDemonstrating faulty behaviour here:")
+    from openvqe.circuit.compiler import compile_controlled_rotation_gate
+    energies = []
+    gradients = []
+    angles = []
+    for n in range(9):
+        theta = n*pi/4
+        # multiplication syntax still follows circuits ... should probably change that at some point
+        U = X(target=1)*Ry(target=0, control=1, angle=theta)
+        U = compile_controlled_rotation_gate(gate=U) # that actually works
+        # leaving this here to demonstrate how to use cirqs ascii print since we don't have our own right now
+        print(SimulatorCirq().create_circuit(abstract_circuit=U))
+        O = Objective(observable=H, unitaries=U)
+        E = SimulatorCirq().expectation_value(objective=O)
+        energies.append(E)
+        dE0 = SimulatorCirq().expectation_value(objective=grad(O)[0])
+        dE1 = SimulatorCirq().expectation_value(objective=grad(O)[1])
+        gradients.append(0.5*(dE0-dE1)) # with this it works
+        angles.append(theta)
+
+    print("Gradient works again")
+    for i,a in enumerate(angles):
+        print(angles[i], " ", energies[i], " ", gradients[i])
+
+    from matplotlib import pyplot as plt
+    plt.title("Gradient Test for controlled Rotation -- manually")
     plt.xlabel("t")
     plt.plot(angles, energies, label="E")
     plt.plot(angles, gradients, label="dE/dt")
