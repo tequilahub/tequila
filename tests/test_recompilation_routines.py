5da4819da8763d40c0e56ffda538c54d3e3938da
Jakob S Kottmann <jakob.kottmann@gmail.com>
2019-09-25 18:37:53 -0400

    fixed recompilation test

--- tests/test_recompilation_routines.py
+++ tests/test_recompilation_routines.py
@@ -11,7 +11,7 @@ def test_controlled_rotations():
         for gate in gs:
             qubit = randint(0, 1)
             control = randint(2, 3)
-            U = gate(target=qubit, control=control, angle=angle)*gates.X(target=control)
+            U = gates.X(target=control)*gate(target=qubit, control=control, angle=angle)
             RCU = compile_controlled_rotation_gate(gate=U)
             wfn1 = SimulatorCirq().simulate_wavefunction(abstract_circuit=U, initial_state=0).wavefunction
             wfn2 = SimulatorCirq().simulate_wavefunction(abstract_circuit=RCU, initial_state=0).wavefunction
