fd2dc610b3865f5263fd5b32c2f20181827e3c6d
Cyrille Lavigne <cyrille.lavigne@mail.utoronto.ca>
2019-11-06 08:37:49 -0500

    Started generic optimizer that uses scipy

---
+++ openvqe/optimizers/scipy_optimizers.py
@@ -0,0 +1,11 @@
+from openvqe import OpenVQEException
+from openvqe import typing
+from openvqe.circuit.variable import Variable
+from openvqe.objective import Objective
+from openvqe.simulator import pick_simulator
+from openvqe.circuit.gradient import grad
+from openvqe import scipy
+from openvqe import numpy
+
+def optimize():
+    pass
