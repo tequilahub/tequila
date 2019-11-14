4527f02d2a43b5361883ae1fa972fe5b0cd4c2cb
Jakob S Kottmann <jakob.kottmann@gmail.com>
2019-11-13 22:37:19 -0500

    more convenience in optimizer handling -> might should move to ordereddicts for parameters. Restructured examples

--- openvqe/optimizers/optimizer_base.py
+++ openvqe/optimizers/optimizer_base.py
@@ -9,28 +9,86 @@ from openvqe.objective import Objective
 from openvqe.simulator import pick_simulator
 from dataclasses import dataclass, field
 
+
 class OpenVQEOptimizerException(OpenVQEException):
     pass
+
 
 @dataclass
 class OptimizerHistory:
 
+    @property
+    def iterations(self):
+        if self.energies is None:
+            return 0
+        else:
+            return len(self.energies)
+
     energies: typing.List[numbers.Real] = field(default_factory=list)
-    gradients: typing.List[typing.Dict[str,numbers.Real]]= field(default_factory=list)
-    angles: typing.List[typing.Dict[str,numbers.Number]]= field(default_factory=list)
+    gradients: typing.List[typing.Dict[str, numbers.Real]] = field(default_factory=list)
+    angles: typing.List[typing.Dict[str, numbers.Number]] = field(default_factory=list)
 
     def extract_energies(self):
         return self.energies
 
-    def extract_gradients(self, key:str):
+    def extract_gradients(self, key: str):
         return [d[key] for d in self.gradients]
 
-    def extract_angles(self, key:str):
+    def extract_angles(self, key: str):
         return [d[key] for d in self.angles]
+
+    def plot(self, filename=None,
+             property: typing.Union[str, typing.List[str]] = 'energies',
+             key: str = None, *args, **kwargs):
+        """
+        Convenience function to plot the progress of the optimizer
+        :param filename: if given plot to file, otherwise plot to terminal
+        :param property: the property to plot, given as string
+        :param key: for properties like angles and gradients you can specifiy which one you want to plot
+        if set to none all keys are plotted
+        give key as list if you want to plot multiple properties with different keys
+        """
+        from matplotlib import pyplot as plt
+
+        properties = None
+        if hasattr(property, "lower"):
+            properties = [property.lower()]
+        else:
+            properties = property
+
+        keys = key
+        if key is None:
+            keys = [[k for k in self.angles[-1].keys()]] * len(properties)
+        elif hasattr(key, "lower"):
+            keys = [[key.lower()]] * len(properties)
+        else:
+            keys = [key]*len(properties)
+
+        for i, p in enumerate(properties):
+            data = None
+            if p.lower() == "energies":
+                data = self.energies
+                plt.plot(data, label=p, marker='o', linestyle='--')
+            else:
+                for k in keys[i]:
+                    data = getattr(self, "extract_" + p)(key=k)
+                    plt.plot(data, label=p+" "+k, marker='o', linestyle='--')
+
+
+        plt.legend()
+        plt.show()
+
 
 class Optimizer:
 
-    def __init__(self, simulator: typing.Type = None, maxiter: int=None, samples: int = None, save_history: bool = True):
+    def __init__(self, simulator: typing.Type = None, maxiter: int = None, samples: int = None,
+                 save_history: bool = True):
+        """
+        :param simulator: The simulator to use (initialized or uninitialized)
+        :param maxiter: Maximum number of iterations
+        :param samples: Number of Samples for the Quantum Backend takes (None means full wavefunction simulation)
+        :param save_history: Save the optimization history in self.history
+        """
 
         if isinstance(simulator, type):
             self.simulator = simulator()
@@ -49,12 +107,17 @@ class Optimizer:
         else:
             self.history = None
 
-    def __call__(self, objective: Objective, initial_values: typing.Dict[str, numbers.Number] = None) -> typing.Dict[str, numbers.Number]:
+    def reset_history(self):
+        self.history = OptimizerHistory()
+
+    def __call__(self, objective: Objective,
+                 initial_values: typing.Dict[str, numbers.Number] = None) -> typing.Tuple[
+        numbers.Number, typing.Dict[str, numbers.Number]]:
         """
         Will try to solve and give back optimized parameters
         :param objective: openvqe Objective object
         :param parameters: initial parameters, if none the optimizers uses what was set in the objective
-        :return: optimal parameters
+        :return: tuple of optimial energy and optimal parameters
         """
         raise OpenVQEOptimizerException("Try to call BaseClass")
 
@@ -64,44 +127,6 @@ class Optimizer:
         :return: updated parameters
         """
         raise OpenVQEOptimizerException("Try to call BaseClass")
-
-    def plot(self, filename=None, property:typing.Union[str,typing.List[str]]='energies', *args, **kwargs):
-        """
-        Convenience function to plot the progress of the optimizer
-        :param filename: if given plot to file, otherwise plot to terminal
-        """
-        if not self.save_history:
-            raise OpenVQEOptimizerException("You explicitly set save_history to False which means there is no data to plot")
-
-        properties = None
-        if hasattr(property, "len"):
-            properties = property
-        else:
-            properties = [property]
-
-        for p in properties:
-            data = None
-            if p.lower() == "energies":
-                data = self.history.extract_energies()
-            elif "angles" in p.lower():
-                key = p.lower().split(' ')[1]
-                data = self.history.extract_angles(key=key)
-            elif "gradients" in p.lower():
-                key = p.lower().split(' ')[1]
-                print("key=", key)
-                data = self.history.extract_gradients(key=key)
-
-            from matplotlib import pyplot as plt
-            plt.plot(data, label=p, marker='o', linestyle='--')
-            plt.legend()
-            plt.show()
-
-
-
-
-
-
-
 
     def initialize_simulator(self, samples: int):
         if self.simulator is not None:
--- openvqe/optimizers/scipy_optimizers.py
+++ openvqe/optimizers/scipy_optimizers.py
@@ -1,84 +1,83 @@
-from openvqe import typing, numbers, copy
+from openvqe import typing, numbers
 from openvqe.objective import Objective
-from openvqe.simulator import pick_simulator
-from openvqe.circuit.gradient import grad
 from openvqe import scipy
 from openvqe import numpy as np
 from .optimizer_base import Optimizer
-
-
-class EvalContainer:
-
-    def __init__(self, objective, params, eval):
-        self.objective = objective
-        self.params = params
-        self.eval = eval
-        self.N = len(params)
-        self.memory = []
-        self.memory_angles = []
-
-    def E_function(self, p, cache=None):
-        angles = dict((self.params[i], p[i]) for i in range(self.N))
-        self.objective.update_parameters(angles)
-        E = self.eval(self.objective)
-        self.memory.append(E)
-        self.memory_angles.append(angles)
-        cache += [E]
-        return E
-
-    def __call__(self, angle, cache=None):
-        return self.E_function(p=angle, cache=cache)
-
-class GradContainer:
-
-    def __init__(self, objective, params, eval):
-        self.objective = objective
-        self.params = params
-        self.N = len(params)
-        self.eval = eval
-        self.memory = []
-
-    def dE_function(self, p, cache=None):
-        dO = grad(self.objective, self.params)
-        dE_vec = np.zeros(self.N)
-        memory = dict()
-        for i in range(self.N):
-            dO[self.params[i]].update_parameters(dict((self.params[i], p[i]) for i in range(len(self.params))))
-            dE_vec[i] = self.eval(dO[self.params[i]])
-            memory[self.params[i]] = dE_vec[i]
-        self.memory.append(memory)
-        return dE_vec
-
-    def __call__(self, angle, cache=None):
-        return self.dE_function(p=angle, cache=cache)
-
-
-
+from ._scipy_containers import _EvalContainer, _GradContainer
 
 
 class OptimizerSciPy(Optimizer):
+    gradient_free_methods = ['Nelder-Mead', 'COBYLA', 'Powell']
+    gradient_based_methods = ['BFGS', 'CG', 'dogleg']
+
+    def available_methods(self):
+        """
+        :return: All tested available methods
+        """
+        return self.gradient_free_methods + self.gradient_based_methods
 
     def __init__(self, method: str = "BFGS", tol: numbers.Real = 1.e-3, method_options=None, method_bounds=None,
-                 method_constraints=None, use_gradient: bool = True, **kwargs):
+                 method_constraints=None, use_gradient: bool = None, **kwargs):
+        """
+        Optimize a circuit to minimize a given objective using scipy
+        See the Optimizer class for all other parameters to initialize
+        :param method: The scipy method passed as string
+        :param use_gradient: do gradient based optimization
+        :param tol: See scipy documentation for the method you picked
+        :param method_options: See scipy documentation for the method you picked
+        :param method_bounds: See scipy documentation for the method you picked
+        :param method_constraints: See scipy documentation for the method you picked
+        """
         super().__init__(**kwargs)
         self.method = method.upper()
         self.tol = tol
         self.method_options = method_options
         self.method_bounds = method_bounds
-        self.use_gradient = use_gradient
+        if use_gradient is None:
+            if method in self.gradient_based_methods:
+                self.use_gradient = True
+            elif method in self.gradient_free_methods:
+                self.use_gradient = False
+            else:
+                self.use_gradient = True
+        else:
+            self.use_gradient = use_gradient
+
+        if method_options is None:
+            self.method_options = {'maxiter': self.maxiter}
+        else:
+            self.method_options = method_options
+            if 'maxiter' not in method_options:
+                self.method_options['maxiter'] = self.maxiter
+
         if method_constraints is None:
             self.method_constraints = ()
         else:
             self.method_constraints = method_constraints
 
-    def __get_eval_function(self, simulator) -> EvalContainer:
+    def __get_eval_function(self, simulator) -> _EvalContainer:
         if self.samples is None:
             return simulator.simulate_objective
         else:
             return simulator.measure_objective
 
-    def __call__(self, objective: Objective, initial_values: typing.Dict[str, numbers.Number] = None) -> typing.Dict[
-        str, numbers.Number]:
+    def __call__(self, objective: Objective,
+                 initial_values: typing.Dict[str, numbers.Number] = None,
+                 return_scipy_output: bool = False,
+                 reset_history: bool = True) -> typing.Tuple[numbers.Number, typing.Dict[str, numbers.Number]]:
+        """
+        Optimizes with scipi and gives back the optimized angles
+        Get the optimized energies over the history
+        :param objective: The openvqe Objective to minimize
+        :param initial_values: initial values for the objective
+        :param return_scipy_output: chose if the full scipy output shall be returned
+        :param reset_history: reset the history before optimization starts (has no effect if self.save_history is False)
+        :return: tuple of optimized energy ,optimized angles and scipy output
+        """
+
+        if self.save_history and reset_history:
+            self.reset_history()
+
         simulator = self.initialize_simulator(self.samples)
 
         # Generate the function that evaluates <O>
@@ -90,142 +89,38 @@ class OptimizerSciPy(Optimizer):
             angles = objective.extract_parameters()
 
         # Transform the initial value directory into (ordered) arrays
-        params, x0 = zip(*angles.items())
-        x0 = np.array(x0)
+        param_keys, param_values = zip(*angles.items())
+        param_values = np.array(param_values)
 
         # Make E, grad E
         dE = None
         Es = []
-        E = EvalContainer(objective=objective, params=params, eval=sim_eval)
+        E = _EvalContainer(objective=objective, param_keys=param_keys, eval=sim_eval, save_history=self.save_history)
         if self.use_gradient:
-            dE = GradContainer(objective=objective, params=params, eval=sim_eval)
+            dE = _GradContainer(objective=objective, param_keys=param_keys, eval=sim_eval,
+                                save_history=self.save_history)
 
-        res = scipy.optimize.minimize(E, x0, jac=dE,
+        bounds = None
+        if self.method_bounds is not None:
+            names, bounds = zip(*self.method_bounds.items())
+            assert (names == param_keys)
+
+        res = scipy.optimize.minimize(E, param_values, jac=dE,
                                       args=(Es,),
                                       method=self.method, tol=self.tol,
-                                      bounds=self.method_bounds,
+                                      bounds=bounds,
                                       constraints=self.method_constraints,
                                       options=self.method_options)
 
         if self.save_history:
-            self.history.energies = E.memory
-            self.history.angles = E.memory_angles
+            self.history.energies = E.history
+            self.history.angles = E.history_angles
             if self.use_gradient:
-                self.history.gradients = dE.memory
-            # self.history['angles'].append()
+                self.history.gradients = dE.history
 
-        angles_final = dict((params[i], res.x[i]) for i in range(len(params)))
-        return angles_final
-
-
-def minimize(objective: Objective,
-             initial_values: typing.Dict[str, numbers.Number] = None,
-             use_gradient: bool = True,
-             simulator=None, samples: int = None,
-             method: str = "BFGS", tol: numbers.Real = 1e-3,
-             method_options=None,
-             method_bounds=None,
-             method_constraints=(),
-             return_all: bool = True) -> typing.Tuple[
-    numbers.Number, typing.Dict[str, numbers.Number], typing.Union[scipy.optimize.OptimizeResult, None]]:
-    """
-    Optimize a circuit to minimize a given objective using scipy
-    :param objective: The openvqe Objective to minimize
-    :param initial_values: initial values for the objective
-    :param use_gradient: do gradient based optimization
-    :param simulator: choose a specific simulator as backend, if None it will be automatically picked
-    :param samples: number of individual measurment samples for each step in the optimization. None means full wavefunction simulation
-    :param method: The scipy method passed as string
-    :param tol: See scipy documentation for the method you picked
-    :param method_options: See scipy documentation for the method you picked
-    :param method_bounds: See scipy documentation for the method you picked
-    :param method_constraints: See scipy documentation for the method you picked
-    :param return_all: return the whole scipy OptimizeResult object
-    :return: tuple(final value of the Objective, optimized parameters, scipy OptimizeResult object or None)
-    """
-
-    # Start the simulator
-    if simulator is None:
-        sim = pick_simulator(samples=samples)
-    else:
-        sim = simulator
-    if isinstance(sim, type):
-        sim = sim()  # Make sure the simulator is initialized
-
-    # Generate the function that evaluates <O>
-    if samples is None:
-        sim_eval = lambda O: sim.simulate_objective(objective=O)
-    else:
-        sim_eval = lambda O: sim.measure_objective(objective=O, samples=samples)
-
-    # Extract initial values
-    angles = initial_values
-    if angles is None:
-        angles = objective.extract_parameters()
-
-    # Transform the initial value directory into (ordered) arrays
-    params, x0 = zip(*angles.items())
-    x0 = np.array(x0)
-
-    # Make E, grad E
-    Es = []
-    E = wrap_energy_function(objective, sim_eval, params)
-    if use_gradient:
-        dE = wrap_energy_gradient_function(objective, sim_eval, params)
-    else:
-        dE = None
-
-    if minimize:
-        res = scipy.optimize.minimize(E, x0, jac=dE,
-                                      args=(Es,),
-                                      method=method, tol=tol,
-                                      bounds=method_bounds,
-                                      constraints=method_constraints,
-                                      options=method_options)
-
-    # Format output
-    res.parameters = params[:]  # add the ordered parameter list to res
-    res.evals = Es[:]  # function evaluations
-    angles_final = dict((params[i], res.x[i]) for i in range(len(params)))
-
-    if return_all:
-        return res.fun, angles_final, res
-    else:
-        return res.fun, angles_final
-
-
-def wrap_energy_function(objective: Objective, simulator_eval, params) -> typing.Callable:
-    """
-    Helper Function for minimize/maximize
-    Generates the scalar function E(theta_1, theta_2 ...).
-    """
-    N = len(params)
-
-    def E_function(p, cache=None):
-        objective.update_parameters(dict((params[i], p[i])
-                                         for i in range(N)))
-        E = simulator_eval(objective)
-        cache += [E]
-        return E
-
-    return E_function
-
-
-def wrap_energy_gradient_function(objective: Objective, simulator_eval, params) -> typing.Callable:
-    """
-    Helper Function for minimize/maximize
-    Generates the vector function \nabla E(theta_1, theta_2 ...).
-    """
-
-    N = len(params)
-    dO = grad(objective, params)
-
-    def dE_function(p, cache=None):
-        dE_vec = np.zeros(N)
-        for i in range(N):
-            dO[params[i]].update_parameters(dict((params[i], p[i])
-                                                 for i in range(len(params))))
-            dE_vec[i] = simulator_eval(dO[params[i]])
-        return dE_vec
-
-    return dE_function
+        E_final = res.fun
+        angles_final = dict((param_keys[i], res.x[i]) for i in range(len(param_keys)))
+        if return_scipy_output:
+            return E_final, angles_final, res
+        else:
+            return E_final, angles_final
