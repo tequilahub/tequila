d1d809bce03c4495c2c9b45fb90bf01792bd4762
Jakob S Kottmann <jakob.kottmann@gmail.com>
2019-11-28 12:58:14 -0500

    fixed minor bugs

---
+++ src/tequila/utils/__init__.py
@@ -0,0 +1,2 @@
+from tequila.utils.bitstrings import BitString, BitStringLSB, BitNumbering, initialize_bitstring
+from tequila.utils.exceptions import TequilaException, TequilaTypeError, TequilaParameterError
