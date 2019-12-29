5e2eda5fb4ba5271c82793d82d4d6ee6b566a6d7
thomsonyen <thomsonyen19981114@gmail.com>
2019-12-28 19:45:05 -0500

    Enable transformation to the qubit-wise commuting form through binary representation

--- src/tequila/grouping/binary_rep.py
+++ src/tequila/grouping/binary_rep.py
@@ -1,36 +1,136 @@
 from tequila import TequilaException
-from tequila.hamiltonian import QubitHamiltonian
+from tequila.hamiltonian import QubitHamiltonian, PauliString
+from tequila.grouping.binary_utils import get_lagrangian_subspace, binary_symplectic_inner_product, binary_solve, binary_phase, gen_single_qubit_term
 import numpy as np
 import numbers
 
+
 class BinaryHamiltonian:
-    def __init__(self, hamiltonian: QubitHamiltonian):
-        self.n_qubit = hamiltonian.n_qubits
+    def __init__(self, binary_terms):
+        '''
+        Initiate from a list of Pauli Strings
+        '''
+        self.binary_terms = binary_terms
 
-        self.binary_terms = [
+        self.n_qubit = binary_terms[0].get_n_qubit()
+
+        self.n_term = len(self.binary_terms)
+
+    @classmethod
+    def init_from_qubit_hamiltonian(cls, hamiltonian: QubitHamiltonian):
+        n_qubit = hamiltonian.n_qubits
+        binary_terms = [
             BinaryPauliString(
-                p.binary(self.n_qubit).binary,
-                p.binary(self.n_qubit).coeff) for p in hamiltonian.paulistrings
+                p.binary(n_qubit).binary,
+                p.binary(n_qubit).coeff) for p in hamiltonian.paulistrings
         ]
+        return BinaryHamiltonian(binary_terms)
 
-    def get_binary_matrix(self):
+    def get_binary(self):
         matrix = [p.get_binary() for p in self.binary_terms]
-        return np.array(matrix)
-    
+        return matrix
+
     def get_coeff(self):
         coeff = [p.get_coeff() for p in self.binary_terms]
-        return np.array(coeff)
+        return coeff
 
-    def nullspace(self):
-        pass
+    def get_qubit_wise(self):
+        '''
+        Return the qubit-wise form of the current binary hamiltonian.
+        And the components of corresponding unitary transformation U, 
+        where U = prod_i (1/2) ** (1/2) * (lagrangian_basis[i] + new_basis[i])
+        '''
+        if not self.is_commuting():
+            raise TequilaException(
+                'Not all terms in the Hamiltonians are commuting.')
 
-    def symplectic_gram_schmidt(self):
-        pass 
+        lagrangian_basis = get_lagrangian_subspace(self.get_binary())
+        new_basis = self.get_single_qubit_basis(lagrangian_basis)
+        lagrangian_basis = [BinaryPauliString(p) for p in lagrangian_basis]
+        new_basis = [BinaryPauliString(p) for p in new_basis]
+        qubit_wise_hamiltonian = self.basis_transform(lagrangian_basis,
+                                                      new_basis)
+
+        # Return the basis in terms of Binary Hamiltonian
+        return qubit_wise_hamiltonian, lagrangian_basis, new_basis
+
+    def get_single_qubit_basis(self, lagrangian_basis):
+        '''
+        Find the single_qubit_basis such that single_qubit_basis[i] anti-commutes
+        with lagrangian_basis[i], and commute for all other cases. 
+        '''
+        dim = len(lagrangian_basis)
+
+        # Free Qubits
+        free_qub = [qub for qub in range(dim)]
+        pair = []
+
+        for i in range(dim):
+            cur_pair = self.find_single_qubit_pair(lagrangian_basis[i],
+                                                   free_qub)
+            for j in range(dim):
+                if i != j:
+                    if binary_symplectic_inner_product(
+                            cur_pair, lagrangian_basis[j] == 1):
+                        lagrangian_basis[j] = (lagrangian_basis[i] +
+                                               lagrangian_basis[j]) % 2
+            pair.append(cur_pair)
+        return pair
+
+    def find_single_qubit_pair(self, cur_basis, free_qub):
+        '''
+        Find the single qubit pair that anti-commute with cur_basis such that the single qubit is in free_qub 
+
+        Return: Binary vectors representing the single qubit pair
+        Modify: Pops the qubit used from free_qub
+        '''
+        dim = len(cur_basis) // 2
+        for idx, qub in enumerate(free_qub):
+            for term in range(3):
+                pair = gen_single_qubit_term(dim, qub, term)
+                # if anticommute
+                if (binary_symplectic_inner_product(pair, cur_basis) == 1):
+                    free_qub.pop(idx)
+                    return pair
+
+    def basis_transform(self, old, new):
+        '''
+        Transforms the given hamiltonian from the old basis to the new basis.
+        
+        Return: The transformed Binary hamiltonian
+        '''
+        return BinaryHamiltonian(
+            [p.basis_transform(old, new) for p in self.binary_terms])
+
+    def is_commuting(self):
+        '''
+        Return whether all terms in the Hamiltonian are commuting
+        '''
+        for i in range(self.n_term):
+            for j in range(i + 1, self.n_term):
+                if not self.binary_terms[i].commute(self.binary_terms[j]):
+                    return False
+        return True
 
     def to_qubit_hamiltonian(self):
-        pass
+        qub_ham = QubitHamiltonian.init_zero()
+        for p in self.binary_terms:
+            qub_ham += QubitHamiltonian.init_from_paulistring(
+                p.to_pauli_strings())
+        return qub_ham
+
 
 class BinaryPauliString:
+    def __init__(self, binary_vector=np.array([0, 0]), coeff=1.0):
+        '''
+        Stores a list of binary vectors and a list of corresponding coefficients. 
+        '''
+        self.binary = np.array(binary_vector)
+        self.coeff = coeff
+        self.n_qubit = len(binary_vector) // 2
+        self.is_binary()
+        self.is_coeff()
+
     def is_binary(self):
         if not isinstance(self.binary, np.ndarray):
             raise TequilaException(
@@ -51,7 +151,9 @@ class BinaryPauliString:
         Determine whether the corresponding pauli-strings of 
         the two binary vectors commute. 
         '''
-        inner_product = self.symplectic_inner_product(other)
+        inner_product = binary_symplectic_inner_product(
+            self.binary, other.binary)
+
         if inner_product == 0:
             return True
         elif inner_product == 1:
@@ -60,21 +162,28 @@ class BinaryPauliString:
             raise TequilaException('Computed unexpected inner product. Got ' +
                                    str(inner_product))
 
-    def symplectic_inner_product(self, other):
+    def basis_transform(self, old, new):
         '''
-        Return the symplectic inner product between two BinaryPauli. 
+        Transform the pauli string from old to new binary basis.
 
-        Return: 0 or 1. 
+        Return: Pauli string in the new basis.
         '''
-        if not self.same_n_qubit(other):
-            raise TequilaException(
-                'Two binary vectors given do not share same number of qubits. '
-            )
+        old_basis_coeff = binary_solve([p.get_binary() for p in old],
+                                       self.binary)
+        original_pauli_vec = np.zeros(self.n_qubit * 2)
+        new_pauli_vec = np.zeros(self.n_qubit * 2)
+        phase = 1
+        for i, i_coeff in enumerate(old_basis_coeff):
+            if i_coeff == 1:
+                phase *= binary_phase(original_pauli_vec, old[i].get_binary(),
+                                      self.n_qubit)
+                original_pauli_vec = (original_pauli_vec +
+                                      old[i].get_binary()) % 2
+                new_pauli_vec = (new_pauli_vec + new[i].get_binary()) % 2
 
-        re = self.binary[:self.n_qubit] @ other.binary[self.n_qubit:] \
-            + other.binary[:self.n_qubit] @ self.binary[self.n_qubit:]
-
-        return re % 2
+        new_pauli_str = BinaryPauliString(new_pauli_vec)
+        new_pauli_str.set_coeff(self.coeff / phase)
+        return new_pauli_str
 
     def same_n_qubit(self, other):
         return self.n_qubit == other.n_qubit
@@ -83,20 +192,24 @@ class BinaryPauliString:
         return all(self.binary == other.binary)
 
     def to_pauli_strings(self):
-        pass
+        data = {}
+        for i in range(self.n_qubit):
+            if (self.binary[i] == 1 and self.binary[i + self.n_qubit] == 0):
+                data[i] = 'X'
+            elif (self.binary[i] == 1 and self.binary[i + self.n_qubit] == 1):
+                data[i] = 'Y'
+            elif (self.binary[i] == 0 and self.binary[i + self.n_qubit] == 1):
+                data[i] = 'Z'
+        return PauliString(data, self.coeff)
 
     def get_coeff(self):
         return self.coeff
 
+    def set_coeff(self, new_coeff):
+        self.coeff = new_coeff
+
     def get_binary(self):
         return self.binary
-    
-    def __init__(self, binary_vector=np.array([0, 0]), coeff=1.0):
-        '''
-        Stores a list of binary vectors and a list of corresponding coefficients. 
-        '''
-        self.binary = np.array(binary_vector)
-        self.coeff = coeff
-        self.n_qubit = len(binary_vector) // 2
-        self.is_binary()
-        self.is_coeff()
+
+    def get_n_qubit(self):
+        return self.n_qubit
--- tests/test_binary_pauli.py
+++ tests/test_binary_pauli.py
@@ -10,11 +10,14 @@ def prepare_test_hamiltonian():
     '''
     Return a test hamiltonian and its solution
     '''
-    H = -1.0 * paulis.Z(0) -0.5 * paulis.Z(1) + 0.1 * paulis.X(0)*paulis.X(1) 
-    coeff_sol = np.array([-1.0, -0.5, 0.1])
-    binary_sol = np.array([[0, 0, 1, 0], [0, 0, 0, 1], [1, 1, 0, 0]])
+    H = -1.0 * paulis.Z(0) * paulis.Z(1) - 0.5 * paulis.Y(0) * paulis.Y(
+        1) + 0.1 * paulis.X(0) * paulis.X(1) + 0.2 * paulis.Z(2)
+    coeff_sol = np.array([-1.0, -0.5, 0.1, 0.2])
+    binary_sol = np.array([[0, 0, 0, 1, 1, 0], [1, 1, 0, 1, 1, 0],
+                           [1, 1, 0, 0, 0, 0], [0, 0, 0, 0, 0, 1]])
 
     return H, H.n_qubits, binary_sol, coeff_sol
+
 
 def test_binarypauli_conversion():
     '''
@@ -33,11 +36,11 @@ def test_binarypauli_conversion():
     assert (word3.coeff == coeff_sol[2])
     assert (all(word3.binary == binary_sol[2, :]))
 
+
 def test_binary_pauli():
     '''
     Testing binary form of the pauli strings 
     '''
-
     x1 = BinaryPauliString([1, 0], 1)
     x1_other = BinaryPauliString(np.array([1, 0]), 2.1)
     assert (x1.same_pauli(x1_other))
@@ -50,15 +53,84 @@ def test_binary_pauli():
     yy = BinaryPauliString([1, 1, 1, 1], 2.1 + 2j)
     assert (xx.commute(yy))
 
+
 def test_binary_hamiltonian_initialization():
     '''
     Testing binary form of the hamiltonian
     '''
     H, n_qubits, binary_sol, coeff_sol = prepare_test_hamiltonian()
 
-    H_binary = BinaryHamiltonian(H)
+    H_binary = BinaryHamiltonian.init_from_qubit_hamiltonian(H)
 
-    binary_equal_matrix = H_binary.get_binary_matrix() == binary_sol
+    binary_equal_matrix = np.array(H_binary.get_binary()) == binary_sol
 
     assert (binary_equal_matrix.all())
-    assert (all(H_binary.get_coeff() == coeff_sol))
+    assert (all(np.array(H_binary.get_coeff()) == coeff_sol))
+
+
+def test_to_qubit_hamiltonian():
+    '''
+    Testing transformation to qubit hamiltonian 
+    '''
+    H, n_qubits, binary_sol, coeff_sol = prepare_test_hamiltonian()
+
+    binary_hamiltonian = BinaryHamiltonian.init_from_qubit_hamiltonian(H)
+
+    assert (equal_qubit_hamiltonian(H,
+                                    binary_hamiltonian.to_qubit_hamiltonian()))
+
+
+def test_single_qubit_basis_transfrom():
+    '''
+    Testing whether transformations using the binary form 
+    and the transformation through direct computation agree
+    '''
+    H, n_qubits, binary_sol, coeff_sol = prepare_test_hamiltonian()
+
+    single_qub_H, old_basis, new_basis = BinaryHamiltonian.init_from_qubit_hamiltonian(
+        H).get_qubit_wise()
+
+    H_brute_force = brute_force_transformation(H, old_basis, new_basis)
+
+    assert (equal_qubit_hamiltonian(single_qub_H.to_qubit_hamiltonian(),
+                                    H_brute_force))
+
+    H = -1.0 * paulis.X(0) * paulis.X(1) * paulis.X(2) + 2.0 * paulis.Y(
+        0) * paulis.Y(1)
+
+    single_qub_H, old_basis, new_basis = BinaryHamiltonian.init_from_qubit_hamiltonian(
+        H).get_qubit_wise()
+
+    H_brute_force = brute_force_transformation(H, old_basis, new_basis)
+
+    assert (equal_qubit_hamiltonian(single_qub_H.to_qubit_hamiltonian(),
+                                    H_brute_force))
+
+
+def brute_force_transformation(H, old_basis, new_basis):
+    def pair_unitary(a, b):
+        '''
+        Accepts a BinaryPauliString. 
+        Return the paired unitary 1/sqrt(2) (a + b) in qubit hamiltonian. 
+        '''
+        a = QubitHamiltonian.init_from_paulistring(a.to_pauli_strings())
+        b = QubitHamiltonian.init_from_paulistring(b.to_pauli_strings())
+        return (1 / 2)**(1 / 2) * (a + b)
+
+    U = QubitHamiltonian.init_unit()
+    for i, i_basis in enumerate(old_basis):
+        U *= pair_unitary(i_basis, new_basis[i])
+
+    return U * H * U
+
+
+def equal_qubit_hamiltonian(a, b):
+    tiny = 1e-6
+    for key, value in a.items():
+        if key in b.keys():
+            if not (abs(value - b[key]) < tiny):
+                return False
+        else:
+            if not (abs(value) < tiny):
+                return False
+    return True
