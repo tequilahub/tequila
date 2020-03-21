5a5c7407e50359dbb274b0cd0b36b0a1601b7c5d
Jakob S Kottmann <jakob.kottmann@gmail.com>
2020-03-20 22:51:56 -0400

    improved paulis

--- src/tequila/hamiltonian/paulis.py
+++ src/tequila/hamiltonian/paulis.py
@@ -6,6 +6,7 @@ Currently uses OpenFermion as backend (QubitOperators)
 import typing
 from tequila.hamiltonian import QubitHamiltonian
 from tequila import BitString
+from tequila.wavefunction.qubit_wavefunction import QubitWaveFunction
 import numpy
 
 
@@ -91,6 +92,7 @@ def I(*args, **kwargs) -> QubitHamiltonian:
     """
     return QubitHamiltonian.init_unit()
 
+
 def Zero(*args, **kwargs) -> QubitHamiltonian:
     """
     Initialize 0 Operator
@@ -101,6 +103,7 @@ def Zero(*args, **kwargs) -> QubitHamiltonian:
 
     """
     return QubitHamiltonian.init_zero()
+
 
 def Qp(qubit) -> QubitHamiltonian:
     """
@@ -201,7 +204,11 @@ def Projector(wfn, threshold=0.0) -> QubitHamiltonian:
 
     Parameters
     ----------
-    wfn: QubitWaveFunction
+    wfn: QubitWaveFunction :
+        The wavefunction onto which the projector projects
+        Needs to be passed down as tequilas QubitWaveFunction type
+        See the documentation on how to initialize a QubitWaveFunction from
+        integer, string or array
 
     threshold: float: (Default value = 0.0)
         neglect small parts of the operator
@@ -210,6 +217,10 @@ def Projector(wfn, threshold=0.0) -> QubitHamiltonian:
     -------
 
     """
+    if isinstance(wfn, str):
+        wfn = QubitWaveFunction.from_string(string=wfn)
+    elif isinstance(wfn, numpy.ndarray):
+        wfn = QubitWaveFunction.from_array(arr=wfn)
     H = QubitHamiltonian.init_zero()
     for k1, v1 in wfn.items():
         for k2, v2 in wfn.items():
@@ -220,9 +231,85 @@ def Projector(wfn, threshold=0.0) -> QubitHamiltonian:
     return H
 
 
+def KetBra(ket: QubitWaveFunction, bra: QubitWaveFunction, hermitian: bool = False, threshold: float = 1.e-6):
+    """
+    Notes
+    ----------
+    Initialize the general KetBra operator
+    .. math::
+        H = \\lvert ket \\rangle \\langle bra \\rvert
+
+    e.g.
+    wfn1 = tq.QubitWaveFunction.from_string("1.0*|00> + 1.0*|11>").normalize()
+    wfn2 = tq.QubitWaveFunction.from_string("1.0*|00>")
+    operator = tq.paulis.KetBra(ket=wfn1, bra=wfn1)
+    initializes the transfer operator from the all-zero state to a Bell state
+
+    Parameters
+    ----------
+    ket: QubitWaveFunction:
+         QubitWaveFunction which defines the ket element
+         can also be given as string or array
+    bra: QubitWaveFunction:
+         QubitWaveFunction which defines the bra element
+         can also be given as string or array
+    hermitian: bool: (Default False)
+         if True the hermitian version H + H^\dagger is returned
+    threshold: float: (Default 1.e-6)
+         elements smaller than the threshold will be ignored
+
+    Returns
+    -------
+    a tequila QubitHamiltonian (not necessarily hermitian)
+
+    """
+    H = QubitHamiltonian.init_zero()
+    if isinstance(ket, str):
+        ket = QubitWaveFunction.from_string(string=ket)
+    elif isinstance(ket, numpy.ndarray):
+        ket = QubitWaveFunction.from_array(arr=ket)
+    if isinstance(bra, str):
+        bra = QubitWaveFunction.from_string(string=bra)
+    elif isinstance(bra, numpy.ndarray):
+        ket = QubitWaveFunction.from_array(arr=bra)
+    for k1, v1 in bra.items():
+        for k2, v2 in ket.items():
+            c = v1.conjugate() * v2
+            if not numpy.isclose(c, 0.0, atol=threshold):
+                H += c * decompose_transfer_operator(bra=k1, ket=k2)
+    if hermitian:
+        return H.split()[0]
+    else:
+        return H.simplify(threshold=threshold)
+
+
 def decompose_transfer_operator(ket: BitString, bra: BitString, qubits: typing.List[int] = None) -> QubitHamiltonian:
     """
-    Decompose |ket><bra| into paulistrings
+    Notes
+    ----------
+    Create the operator
+
+    Note that this is operator is not necessarily hermitian
+    So be careful when using it as a generator for gates
+
+    e.g.
+    decompose_transfer_operator(ket="01", bra="10", qubits=[2,3])
+    gives the operator
+
+    .. math::
+        \\lvert 01 \\rangle \\langle 10 \\rvert_{2,3}
+
+    acting on qubits 2 and 3
+
+    Parameters
+    ----------
+    ket: pass an integer, string, or tequila BitString
+    bra: pass an integer, string, or tequila BitString
+    qubits: pass the qubits onto which the operator acts
+
+    Returns
+    -------
+
     """
 
     opmap = {
