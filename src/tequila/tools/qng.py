a21f2a04525e7b3814585ff7dd26d90cb039983b
Sumner Alperin-Lea <sumner.alperin@mail.utoronto.ca>
2020-04-02 15:45:20 -0400

    current situation on the qng.

--- src/tequila/tools/qng.py
+++ src/tequila/tools/qng.py
@@ -54,3 +54,53 @@ def qng_metric_tensor_blocks(objective,initial_values,samples=None,backend=None,
                     block[k][q] = compile_objective(arg, variables=initial_values, samples=samples, backend=backend,noise_model=noise_model)
             blocks.append(block)
     return blocks
+
+def qng_metric_not_blocks(objective,initial_values,samples=None,backend=None,noise_model=None):
+    if len(objective.args) is not 1:
+        raise TequilaException('sorry, no generalized QNG yet')
+
+    U=objective.args[0].U
+    vars=U.extract_variables()
+    pos_match={i:vars[i] for i in range(len(vars))}
+    moments=U.canonical_moments
+    sub=[QCircuit.from_moments(moments[:i]) for i in range(1,len(moments),2)]
+    parametric_moms=[moments[i] for i in range(1,len(moments)+1,2)]
+    generators =[]
+    for pm in parametric_moms:
+        set=[]
+        if len(pm.gates) is not 0:
+            for gate in pm.gates:
+                gen=get_generator(gate)
+                set.append((gate.parameter,gen))
+        if len(set) is not 0:
+            generators.append(set)
+        else:
+            generators.append(None)
+    lol=[[0 for i in range(len(vars))] for i in range(len(vars))]
+    for i,set in enumerate(generators):
+        if set is None:
+            pass
+        else:
+            for k,pair_1 in enumerate(set):
+                for q,pair_2 in enumerate(set):
+                    gen1=pair_1[1]
+                    gen2=pair_2[1]
+                    if k == q:
+                        arg= (ExpectationValue(U=sub[i], H=gen1 * gen1) - ExpectationValue(U=sub[i],H=gen1)**2)/4
+                    else:
+                        arg = (ExpectationValue(U=sub[i], H=gen1 * gen2) - ExpectationValue(U=sub[i], H=gen1)*ExpectationValue(U=sub[i],H=gen2) ) / 4
+                    pos_1=pos_match[pair_1[0]]
+                    pos_2=pos_match[pair_2[0]]
+                    lol[pos_1][pos_2] += compile_objective(arg, variables=initial_values, samples=samples, backend=backend,noise_model=noise_model)
+
+    return lol
+
+
+def expectation_value_operation(objective,initial_values,samples=None,backend=None,noise_model=None):
+    args= objective.args
+
+    for i,arg in enumerate(args):
+        if hasattr(arg,'U'):
+            blocks=qng_metric_tensor_blocks(arg,initial_values,samples,backend,noise_model)
+            vs= arg.U.extract_variables()
+            gradient_dict= []
