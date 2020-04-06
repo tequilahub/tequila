217099e465bdb55bfcb6a3933692197ef6124aee
Jakob S Kottmann <jakob.kottmann@gmail.com>
2020-04-06 17:31:11 -0400

    added cleanup to tests

---
+++ tests/test_zzz_cleanup.py
@@ -0,0 +1,10 @@
+import os, glob
+
+
+def teardown_function(function):
+    [os.remove(x) for x in glob.glob("*.npy")]
+    [os.remove(x) for x in glob.glob("qvm.log")]
+    [os.remove(x) for x in glob.glob("*.dat")]
+
+def test_cleanup():
+    pass
