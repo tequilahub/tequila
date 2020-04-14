df7794135a515a35fd64fb48d0c033f5b44cd957
thomsonyen <thomsonyen19981114@gmail.com>
2020-04-13 20:13:12 -0400

    Add tests for graph coloring algorithms

--- tests/test_binary_utils.py
+++ tests/test_binary_utils.py
@@ -1,4 +1,4 @@
-from tequila.grouping.binary_utils import get_lagrangian_subspace, binary_null_space, binary_symplectic_gram_schmidt, binary_symplectic_inner_product, binary_symplectic_orthogonalization, pick_symplectic_pair
+from tequila.grouping.binary_utils import get_lagrangian_subspace, binary_null_space, binary_symplectic_gram_schmidt, binary_symplectic_inner_product, binary_symplectic_orthogonalization, pick_symplectic_pair, largest_first, recursive_largest_first
 import numpy as np
 import itertools
 
@@ -103,3 +103,32 @@ def is_binary_basis(basis, vec):
         equal_lst.append(all(cur_comb == vec))
 
     return any(equal_lst)
+
+
+def test_graph_coloring():
+    # anti-commutation graph has either 1, or 0. 0 on the diagonal
+    num_elem = 5
+    cg = np.random.randint(2, size=(num_elem, num_elem))
+    np.fill_diagonal(cg, 1)
+    for i in range(num_elem):
+        for j in range(i + 1, num_elem):
+            cg[j, i] = cg[i, j]
+
+    lf_grouping = largest_first(np.arange(num_elem), num_elem, cg)
+    rlf_grouping = recursive_largest_first(np.arange(num_elem), num_elem, cg)
+
+    assert verify_graph_coloring(cg, lf_grouping)
+    assert verify_graph_coloring(cg, rlf_grouping)
+
+
+def verify_graph_coloring(cg, grouping):
+    # Check if all terms within each group are disconnected
+    for key in grouping.keys():
+        cur_group = grouping[key]
+        num_group = len(cur_group)
+        for i in range(num_group):
+            for j in range(i+1, num_group):
+                idx1, idx2 = cur_group[i], cur_group[j]
+                if cg[idx1][idx2] == 1:
+                    return False
+    return True
