4c1db1c7fc74cf15875798fe4473a2b3839b1d1a
J. S. Kottmann <jakob.kottmann@gmail.com>
2021-02-17 00:24:49 -0500

    Update test_gpyopt.py

--- tests/test_gpyopt.py
+++ tests/test_gpyopt.py
@@ -29,7 +29,7 @@ def test_execution(simulator):
 
     H = 1.0 * tq.paulis.X(0) + 2.0 * tq.paulis.Y(1) + 3.0 * tq.paulis.Z(2)
     O = tq.ExpectationValue(U=U, H=H)
-    result = tq.minimize(method="lbfgs", objective=O, maxiter=1, backend=simulator)
+    result = tq.minimize(method="gpyopt-lbfgs", objective=O, maxiter=1, backend=simulator)
 
 @pytest.mark.skipif(condition=not has_gpyopt, reason="you don't have GPyOpt")
 @pytest.mark.parametrize("simulator", samplers)
@@ -45,12 +45,12 @@ def test_execution_shot(simulator):
     H = 1.0 * tq.paulis.X(0) + 2.0 * tq.paulis.Y(1) + 3.0 * tq.paulis.Z(2)
     O = tq.ExpectationValue(U=U, H=H)
     mi=2
-    result = tq.minimize(method="lbfgs",objective=O, maxiter=mi, backend=simulator,samples=1024)
+    result = tq.minimize(method="gpyopt-lbfgs",objective=O, maxiter=mi, backend=simulator,samples=1024)
     print(result.history.energies)
 
 @pytest.mark.skipif(condition=not has_gpyopt, reason="you don't have GPyOpt")
 @pytest.mark.parametrize("simulator", simulators)
-@pytest.mark.parametrize('method',['lbfgs','DIRECT','CMA'])
+@pytest.mark.parametrize('method',['gpyopt-lbfgs','gpyopt-DIRECT','gpyopt-CMA'])
 def test_one_qubit_wfn(simulator,method):
     U = tq.gates.Trotterized(angles=["a"], steps=1, generators=[tq.paulis.Y(0)])
     H = tq.paulis.X(0)
@@ -65,7 +65,7 @@ def test_one_qubit_wfn_really_works(simulator):
     U = tq.gates.Trotterized(angles=["a"], steps=1, generators=[tq.paulis.Y(0)])
     H = tq.paulis.X(0)
     O = tq.ExpectationValue(U=U, H=H)
-    result = tq.minimize(method="lbfgs",objective=O, maxiter=8, backend=simulator)
+    result = tq.minimize(method="gpyopt-lbfgs",objective=O, maxiter=8, backend=simulator)
     assert (numpy.isclose(result.energy, -1.0,atol=1.e-2))
     assert (numpy.isclose(result.energy,simulate(objective=O,variables=result.angles)))
 
@@ -76,5 +76,5 @@ def test_one_qubit_shot(simulator):
     U = tq.gates.Trotterized(angles=["a"], steps=1, generators=[tq.paulis.Y(0)])
     H = tq.paulis.X(0)
     O = tq.ExpectationValue(U=U, H=H)
-    result = tq.minimize(method="lbfgs",objective=O, maxiter=100, backend=simulator, samples=10000, initial_values=numpy.pi/2)
+    result = tq.minimize(method="gpyopt-lbfgs",objective=O, maxiter=100, backend=simulator, samples=10000, initial_values=numpy.pi/2)
     assert (numpy.isclose(result.energy, -1.0, atol=1.e-1))
