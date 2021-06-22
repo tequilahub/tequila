0788a03c03179a7006599a634e7201dd618c9688
J. S. Kottmann <jakob.kottmann@gmail.com>
2021-06-22 09:43:11 -0400

    more convenience in adding gates to circuit (#157)

--- src/tequila/circuit/circuit.py
+++ src/tequila/circuit/circuit.py
@@ -368,7 +368,6 @@ class QCircuit():
         return not (self.is_fully_parametrized() or self.is_fully_unparametrized())
 
     def __iadd__(self, other):
-        # duck typing
         other = self.wrap_gate(gate=other)
 
         offset = len(self.gates)
@@ -381,6 +380,7 @@ class QCircuit():
         return self
 
     def __add__(self, other):
+        other = self.wrap_gate(other)
         gates = [g.copy() for g in (self.gates + other.gates)]
         result = QCircuit(gates=gates)
         result._min_n_qubits = max(self._min_n_qubits, other._min_n_qubits)
