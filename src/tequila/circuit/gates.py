9bae05c8574f190dfb918be61799e28a9fa57b93
Jakob S Kottmann <jakob.kottmann@gmail.com>
2021-10-12 14:28:33 -0400

    sign consistency between upccgsd circuit optimizations

--- src/tequila/circuit/gates.py
+++ src/tequila/circuit/gates.py
@@ -1036,7 +1036,7 @@ class QubitExcitationImpl(impl.DifferentiableGateImpl):
             U0 = X(target=p)
             U0 += X(target=p, control=q)
             U0 += X(target=p)
-            U1 = Ry(angle=self.parameter, target=q, control=p)
+            U1 = Ry(angle=-self.parameter, target=q, control=p)
             return U0 + U1 + U0
         elif self.compile_options == "optimize" and len(self.target) == 4:
             p,r,q,s = self.target
@@ -1045,7 +1045,7 @@ class QubitExcitationImpl(impl.DifferentiableGateImpl):
             U0 += X(target=r, control=p)
             U0 += X(target=q)
             U0 += X(target=s)
-            U1 = Ry(angle=self.parameter, target=p, control=[q,r,s])
+            U1 = Ry(angle=-self.parameter, target=p, control=[q,r,s])
             return U0 + U1 + U0.dagger()
         else:
             return Trotterized(angle=self.parameter, generator=self.generator, steps=1)
