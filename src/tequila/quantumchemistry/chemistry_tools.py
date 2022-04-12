057de5f75468d1463ac825f384c19ecc77854a28
J. S. Kottmann <jakob.kottmann@gmail.com>
2022-04-12 09:30:07 -0400

    formatting

--- src/tequila/quantumchemistry/chemistry_tools.py
+++ src/tequila/quantumchemistry/chemistry_tools.py
@@ -604,6 +604,9 @@ class NBodyTensor:
     def shape(self, *args, **kwargs):
         return self.elems.shape
 
+    def __getitem__(self, item):
+        return self.elems[item]
+
     def sub_lists(self, idx_lists: list = None) -> numpy.ndarray:
         """
         Get subspace of tensor by a set of index lists
