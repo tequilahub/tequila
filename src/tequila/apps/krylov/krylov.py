8795169111063c1fb50f6c6b976828948ec3bbaa
fran-scala <scala0francesco@gmail.com>
2022-05-26 14:21:33 +0200

    Add assume_real, *args, **kwargs

--- src/tequila/apps/krylov/krylov.py
+++ src/tequila/apps/krylov/krylov.py
@@ -5,17 +5,20 @@ from tequila import braket
 from tequila.hamiltonian.qubit_hamiltonian import QubitHamiltonian
 
 
-def krylov_method(krylov_circs:list, H:QubitHamiltonian, variables:dict=None)->tuple:
+def krylov_method(krylov_circs:list, H:QubitHamiltonian, variables:dict=None, assume_real:bool=False, *args, **kwargs)->tuple:
     """Function that applies Krylov method to an Hamiltonian operator,
     given the list of Krylov quantum circuits. If the circuits are parametrized 
     also the variables need to be passed. The method returns the ground state energy 
     and the array of coefficients allowing to obtain an approximation of the ground state.
+    Optional function arguments (*args, **kwargs) allows to change simulation options.
 
     Args:
         krylov_circs (list): List of Krylov circuits.
         H (QubitHamiltonian): Hamiltonian on which we want to apply Krylov method
         variables (dict, optional): Dicitionary containing possible variables to be stored in the Krylov circuits. 
         Defaults to None.
+        assume_real (bool): If set to True the function does not compute the imaginary part.
+        Default to False.
 
     Returns:
         tuple(float, np.ndarray): ground state energy, krylov coefficients
@@ -25,23 +28,27 @@ def krylov_method(krylov_circs:list, H:QubitHamiltonian, variables:dict=None)->t
     HM = tq.QTensor(shape=[n_krylov_states,n_krylov_states])
     SM = tq.QTensor(shape=[n_krylov_states,n_krylov_states])
     
-    if variables != None:
+    if variables is not None:
         krylov_circs_x = [U.map_variables(variables) for U in krylov_circs] 
     else:
         krylov_circs_x = copy.deepcopy(krylov_circs)
 
     for i in range(n_krylov_states):
         for j in range(i,n_krylov_states):
-            h_real, h_im = braket(bra=krylov_circs_x[i], ket=krylov_circs_x[j], operator=H)
+            if assume_real:
+                h_real = braket(bra=krylov_circs_x[i], ket=krylov_circs_x[j], operator=H)[0]
+                h_im = 0
+            else:
+                h_real, h_im = braket(bra=krylov_circs_x[i], ket=krylov_circs_x[j], operator=H)
             HM[i,j] = h_real + 1j*h_im
             HM[j,i] = h_real - 1j*h_im
             s_real, s_im = braket(bra=krylov_circs_x[i], ket=krylov_circs_x[j])
             SM[i,j] = s_real + 1j*s_im
             SM[j,i] = s_real - 1j*s_im
 
-    h = tq.simulate(HM) 
-    s = tq.simulate(SM)
+    h = tq.simulate(HM, *args, **kwargs) 
+    s = tq.simulate(SM, *args, **kwargs)
 
     v,vv = scipy.linalg.eigh(h,s)
 
-    return v[0], vv[0]
+    return v, vv
