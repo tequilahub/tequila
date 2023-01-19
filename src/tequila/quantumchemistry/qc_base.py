d41d655cf88a295098a8481a58c215ff35033dc2
J. S. Kottmann <jakob.kottmann@gmail.com>
2023-01-19 13:02:17 +0100

    added UR and UC to chemistry module

--- src/tequila/quantumchemistry/qc_base.py
+++ src/tequila/quantumchemistry/qc_base.py
@@ -312,6 +312,52 @@ class QuantumChemistryBase:
                     indices, self.n_orbitals))
         return gates.QubitExcitation(angle=angle, target=target, assume_real=assume_real, control=control,
                                      compile_options=compile_options)
+    
+    def UR(self,i,j,label=None, *args, **kwargs):
+        """
+        Convenience function for orbital rotation circuit (rotating spatial orbital i and j) with standard naming of variables
+        See arXiv:2207.12421 Eq.6 for UR(0,1)
+        """
+        i,j = self.format_excitation_indices((i,j))
+        angle = ("R",i,j,label)*numpy.pi
+        return make_orbital_rotation_gate(indices=(i,j), angle=angle, *args, **kwargs)
+    
+    def UC(self,i,j,label=None, *args, **kwargs):
+        """
+        Convenience function for orbital correlator circuit (correlating spatial orbital i and j through a spin-paired double excitation) with standard naming of variables
+        See arXiv:2207.12421 Eq.22 for UC(1,2)
+        """
+        i,j = self.format_excitation_indices((i,j))
+        angle = ("R",i,j,label)*numpy.pi
+        if "jordanwigner" in self.transformation.name.lower() and not self.transformation.up_then_down:
+            # for JW we can use the optimized form shown in arXiv:2207.12421 Eq.22
+            return gates.QubitExcitation(target=[2*i,2*j,2*i+1,2*j+1], angle=angle, *args, **kwargs)
+        else:
+            return self.make_excitation_gate(indices=[(2*i,2*j),(2*i+1,2*j+1)], angle=angle, *args, **kwargs)
+
+    def make_orbital_rotation_gate(self, indices:tuple, angle, control:list=None, assume_real:bool=True):
+        """
+        
+        Parameters:
+        ----------
+            indices:
+                tuple of two spatial(!) orbital indices
+            angle:
+                Numeric or hashable type or tequila objective
+            control:
+                List of possible control qubits
+            assume_real:
+                Assume that the wavefunction will always stay real.
+                Will reduce potential gradient costs by a factor of 2
+        """
+
+        assert len(indices) == 2
+        i = indices[0]
+        j = indices[1]
+        U = self.make_excitation_gate(indices=[2*i,2*j],angle=angle, control=control)
+        U+= self.make_excitation_gate(indices=[2*i+1,2*j+1],angle=angle, control=control)
+        return U
+
 
     def make_excitation_gate(self, indices, angle, control=None, assume_real=True, **kwargs):
         """
