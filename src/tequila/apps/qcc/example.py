a582acfcda8e03086405386cb7e6b12841e067df
ral9000 <robert.lang@mail.utoronto.ca>
2023-02-15 19:20:10 -0500

    small changes

--- src/tequila/apps/qcc/example.py
+++ src/tequila/apps/qcc/example.py
@@ -4,19 +4,41 @@ import tequila as tq
 from tequila.apps.qcc.qcc import IterativeQCC
 
 
-molecule = tq.chemistry.Molecule(geometry="H 0.0 0.0 0.0\nLi 0.0 0.0 1.5", basis_set="sto-3g")
+import math
+import tequila as tq
 
-H = molecule.make_hamiltonian()
+R = 1.0
+angle = math.radians(107.6 / 2)
+x = R * math.sin(angle)
+y = R * math.cos(angle)
 
-E_fci = molecule.compute_energy("fci")
-print('FCI energy: {}'.format(E_fci))
+xyz = ''.join(["O 0.0 0.0 0.0\n",
+               "H {}  {}  0.0\n".format(-x, y),
+               "H {}  {}  0.0".format(x, y)
+              ]
+             )
 
-iQCC = IterativeQCC(molecule)
+basis = '6-31g'
+active = {'B1':[0,1], 'A1':[2,3]}
 
-for i in range(10):
-    iQCC.do_iteration(n_gen=1)
-    print('iQCC energy at iter {}: {}'.format(i, iQCC.energy))
+h2o = tq.quantumchemistry.Molecule(geometry=xyz, basis_set = basis, active_orbitals = active)
 
-print('\nEnergy errors for the iQCC iterations:')
-for i in range(10):
-    print('iter {}: {}'.format(i, iQCC.iteration_energies[i] - E_fci))
+energy_fci = h2o.compute_energy('fci') #compute the FCI energy for error analysis later
+print('FCI energy: {}'.format(energy_fci))
+
+iqcc_solver = IterativeQCC(h2o)
+
+n_iter = 3
+
+for _ in range(n_iter):
+    iqcc_solver.do_iteration(n_gen=6)
+
+print('\nL1 norm of gradients at exit: {}'.format(iqcc_solver.grad_norm()))
+
+print('Energy errors (Hartree) relative to the FCI value for the iQCC iterations:')
+for idx in range(n_iter):
+    print('iter {}: {}'.format(idx, iqcc_solver.energies[idx] - energy_fci))
+
+print(iqcc_solver.energies)
+print(iqcc_solver.generators)
+print(iqcc_solver.n_terms)
