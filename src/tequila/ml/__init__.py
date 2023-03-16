d1f3acc96b1e5c81fd6feef0183c375f7af64dc4
J. S. Kottmann <jakob.kottmann@gmail.com>
2023-03-16 11:20:43 +0100

    removing deprecated tensorflow interface

--- src/tequila/ml/__init__.py
+++ src/tequila/ml/__init__.py
@@ -1,6 +1,4 @@
 from .ml_api import to_platform
-from .ml_api import HAS_TORCH, HAS_TF
+from .ml_api import HAS_TORCH
 if HAS_TORCH:
     from .interface_torch import TorchLayer
-if HAS_TF:
-    from .interface_tf import TFLayer
