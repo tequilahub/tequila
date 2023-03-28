4a4545d7f0fccb778b5917bf84f2ad97ca65053e
Rick0317 <rick.hey.huang@gmail.com>
2023-03-28 08:49:47 -0400

    Resolved some issues

--- tests/test_visualize.py
+++ tests/test_visualize.py
@@ -5,6 +5,7 @@ import tequila
 import openfermion
 import matplotlib.pyplot as plt
 
+
 if 'networkx' in sys.modules:
     NETWORKX_EXIST = True
 else:
@@ -24,7 +25,7 @@ else:
 @pytest.mark.skipif(condition=not NETWORKX_EXIST, reason="You don't have networkx")
 @pytest.mark.skipif(condition=not MATPLOTLIB_EXIST, reason="You don't have matplotlib")
 @pytest.mark.skipif(condition=not OPENFERMION_EXIST, reason="You don't have openfermion")
-@pytest.mark.parametrize("qh", [tequila.QubitHamiltonian("X(0)X(5)Y(3)")])
+@pytest.mark.parametrize("qh", [(tequila.QubitHamiltonian("X(0)X(5)Y(3)"))])
 def test_visualize_with_qubit_hamiltonian(qh):
     fig = visualize(qh)
     assert plt.fignum_exists(fig.number)
