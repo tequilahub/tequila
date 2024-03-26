294a5ea803cc2cea8a211a06ce8c74b009ad5d60
Davide Bincoletto <davibinco@gmail.com>
2024-03-26 13:12:28 +0100

    add doc for spin-orbital order

--- src/tequila/quantumchemistry/qc_base.py
+++ src/tequila/quantumchemistry/qc_base.py
@@ -648,6 +648,7 @@ class QuantumChemistryBase:
     def make_annihilation_op(self, orbital, coefficient=1.0):
         """
         Compute annihilation operator on spin-orbital in qubit representation
+        Spin-orbital order is always (up,down,up,down,...)
         """
         assert orbital<=self.n_orbitals*2
         aop = openfermion.ops.FermionOperator(f'{orbital}', coefficient)
@@ -656,6 +657,7 @@ class QuantumChemistryBase:
     def make_creation_op(self, orbital, coefficient=1.0):
         """
         Compute creation operator on spin-orbital in qubit representation
+        Spin-orbital order is always (up,down,up,down,...)
         """
         assert orbital<=self.n_orbitals*2
         cop = openfermion.ops.FermionOperator(f'{orbital}^', coefficient)
@@ -664,6 +666,7 @@ class QuantumChemistryBase:
     def make_number_op(self, orbital):
         """
         Compute number operator on spin-orbital in qubit representation
+        Spin-orbital order is always (up,down,up,down,...)
         """
         num_op = self.make_creation_op(orbital) * self.make_annihilation_op(orbital)
         return num_op
