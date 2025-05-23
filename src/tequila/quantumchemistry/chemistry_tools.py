6a2e8f97aec0f26622c93575ce3d749f57b983b9
J. S. Kottmann <jakob.kottmann@gmail.com>
2025-05-23 22:14:07 +0200

    baby coding

--- src/tequila/quantumchemistry/chemistry_tools.py
+++ src/tequila/quantumchemistry/chemistry_tools.py
@@ -257,17 +257,17 @@ class ParametersQC:
         for atom in self.get_atoms():
             n=self.get_atom_number(atom)
             if n>2:
-                result = 2 
+                result += 2 
             if n>10:
-                result = 10
+                result += 10-2
             if n>18:
-                result = 18
+                result += 18-10-2
             if n>36:
-                result = 36
+                result += 36-18-10-2
             if n>54:
-                result = 54
+                result += 54-36-18-10-2
             if n>86:
-                result = 86
+                result += 86-54-36-18-10-2
         return result
 
     @property
