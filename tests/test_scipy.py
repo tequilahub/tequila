4f1b7c2b3485cdf2d235d4bcf00489f961417e93
Roll249 <80768386+Roll249@users.noreply.github.com>
2025-06-05 10:01:13 +0000

    fix build

--- tests/test_scipy.py
+++ tests/test_scipy.py
@@ -72,7 +72,7 @@ def test_gradient_free_methods(simulator, method):
 
     initial_values = {"a": 0.1, "b": 0.01}
     if method == "SLSQP":  # method is not good
-        return True
+        pytest.skip("SLSQP is not a good method for this test")
 
     result = tq.optimizer_scipy.minimize(objective=-E, method=method, tol=1.e-4, backend=simulator,
                                          initial_values=initial_values, silent=True)
