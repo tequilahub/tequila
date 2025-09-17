5378d7ca3f4450031460bf6a0bd13b21d105a10b
Leander Reascos <49106116+LeanderReascos@users.noreply.github.com>
2025-09-17 15:23:59 +0200

    Fix compute_energy to pass **kwargs to QuantumChemistryPySCF

--- src/tequila/quantumchemistry/qc_base.py
+++ src/tequila/quantumchemistry/qc_base.py
@@ -1939,7 +1939,7 @@ class QuantumChemistryBase:
                 from tequila.quantumchemistry import QuantumChemistryPySCF
 
                 molx = QuantumChemistryPySCF.from_tequila(self)
-                return molx.compute_energy(method=method)
+                return molx.compute_energy(method=method, **kwargs)
 
     def compute_fci(self, *args, **kwargs):
         raise NotImplementedError("compute_fci only implemented for the 'pyscf' backend")
