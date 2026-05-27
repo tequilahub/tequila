c8f264b70844e4e2cf4b0502a3cf7fbe3474675d
ohuettenhofer <oliver.huettenhofer@gmail.com>
2026-05-27 11:34:32 +0200

    Reimplement QCircuit to_matrix function using Tequila simulate

--- src/tequila/circuit/circuit.py
+++ src/tequila/circuit/circuit.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 from tequila.circuit._gates_impl import QGateImpl, assign_variable, list_assignment, GlobalPhaseGateImpl, PhaseGateImpl
 from tequila.utils.exceptions import TequilaException, TequilaWarning
-from tequila.utils.bitstrings import BitNumbering
+from tequila.utils.bitstrings import BitNumbering, reverse_int_bits
 import typing
 import copy
 from collections import defaultdict
@@ -449,7 +449,7 @@ class QCircuit:
         else:
             return QCircuit(gates=[gate])
 
-    def to_matrix(self, variables=None):
+    def to_matrix(self, variables=None, numbering: BitNumbering = BitNumbering.MSB, backend: str = None):
         """
         take the circuit and return the unitary matrix corresponding to it.
         Parameters
@@ -464,60 +464,31 @@ class QCircuit:
         np.ndarray:
             the unitary matrix corresponding to the circuit.
         """
-        import quimb.gates
-        import quimb.tensor as qtn
-        from tequila import compile_circuit
+        from tequila import format_variable_dictionary
+        from tequila.simulators.simulator_api import compile_circuit
 
-        num_variables = len(self.extract_variables())
+        variables = format_variable_dictionary(variables)
 
-        if num_variables == 0:
-            variables = {}
-        elif variables is None:
+        if variables is None and not (len(self.extract_variables()) == 0):
             raise TequilaException(
-                f"QCircuit.to_matrix(): no variables provided, but the circuit has {num_variables} variables"
+                "You called simulate for a parametrized type but forgot to pass down the variables: {}".format(
+                    self.extract_variables()
+                )
             )
 
-        compiled_circuit = compile_circuit(
-            self,
+        compiled = compile_circuit(
+            abstract_circuit=self,
+            variables=variables,
+            backend=backend,
         )
-        compiled_circuit = compiled_circuit.map_variables(variables)
 
-        gate_mapping = {"Rx": "RX", "Ry": "RY", "Rz": "RZ", "H": "H", "X": "X"}
+        columns = []
+        for i in range(2**self.n_qubits):
+            initial_state = reverse_int_bits(i, nbits=self.n_qubits) if numbering == BitNumbering.LSB else i
+            output = compiled.simulate(variables=variables, initial_state=initial_state).to_array(numbering, copy=False)
+            columns.append(output)
 
-        quimb_circuit = qtn.Circuit(self.n_qubits)
-
-        # quimb uses MSB convention, so we need to modify the qubit indices
-        # to LSB
-        for g in compiled_circuit.gates:
-            if g.name not in gate_mapping:
-                raise TequilaException(
-                    f"Gate {g.name} is not supported for conversion to matrix. "
-                    f"Supported gates: {list(gate_mapping.keys())}"
-                )
-
-            if g.is_parameterized():
-                quimb_circuit.apply_gate(
-                    gate_mapping[g.name],
-                    params=[float(g.parameter())],
-                    qubits=[abs(t - self.n_qubits + 1) for t in list(g.target)],
-                    parameterize=True,
-                )
-            elif g.is_controlled():
-                quimb_circuit.apply_gate(
-                    gate_mapping[g.name],
-                    qubits=[abs(t - self.n_qubits + 1) for t in list(g.target)],
-                    controls=[abs(c - self.n_qubits + 1) for c in list(g.control)],
-                )
-            else:
-                quimb_circuit.apply_gate(
-                    gate_mapping[g.name],
-                    qubits=[abs(t - self.n_qubits + 1) for t in list(g.target)],
-                )
-
-        uni = quimb_circuit.get_uni()
-        unitary = np.array(uni.to_dense())
-
-        return unitary
+        return np.column_stack(columns)
 
     def to_networkx(self):
         """
