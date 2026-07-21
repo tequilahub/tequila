fc061c7a0eab98c2dce4798400df8e82084b8e05
JdelArco98 <francisco.del.arco.santos@uni-a.de>
2026-07-21 14:04:13 +0200

    Remove redundant test functions from test_scipy.py
    Removed two test functions for execution and execution with shots in test_scipy.py.

--- tests/test_scipy.py
+++ tests/test_scipy.py
@@ -8,53 +8,6 @@ import select_backends
 simulators = select_backends.get()
 samplers = select_backends.get(sampler=True)
 
-
-@pytest.mark.parametrize("simulator", simulators)
-def test_execution(simulator):
-    U = (
-        tq.gates.Rz(angle="a", target=0)
-        + tq.gates.X(target=2)
-        + tq.gates.Ry(angle="b", target=1, control=2)
-        + tq.gates.Trotterized(
-            angles=["c", "d"], generators=[-0.25 * tq.paulis.Z(1), tq.paulis.X(0) + tq.paulis.Y(1)], steps=2
-        )
-        + tq.gates.Trotterized(
-            angles=[1.0, 2.0], generators=[-0.25 * tq.paulis.Z(1), tq.paulis.X(0) + tq.paulis.Y(1)], steps=2
-        )
-        + tq.gates.ExpPauli(angle="a", paulistring="X(0)Y(1)Z(2)")
-    )
-
-    H = 1.0 * tq.paulis.X(0) + 2.0 * tq.paulis.Y(1) + 3.0 * tq.paulis.Z(2)
-    O = tq.ExpectationValue(U=U, H=H)
-
-    result = tq.optimizer_scipy.minimize(
-        objective=O, options={"maxfun": 2}, method="TNC", backend=simulator, silent=True
-    )
-
-
-@pytest.mark.parametrize("simulator", samplers)
-def test_execution_shot(simulator):
-    U = (
-        tq.gates.Rz(angle="a", target=0)
-        + tq.gates.X(target=2)
-        + tq.gates.Ry(angle="b", target=1, control=2)
-        + tq.gates.Trotterized(
-            angles=["c", "d"], generators=[-0.25 * tq.paulis.Z(1), tq.paulis.X(0) + tq.paulis.Y(1)], steps=2
-        )
-        + tq.gates.Trotterized(
-            angles=[1.0, 2.0], generators=[-0.25 * tq.paulis.Z(1), tq.paulis.X(0) + tq.paulis.Y(1)], steps=2
-        )
-        + tq.gates.ExpPauli(angle="a", paulistring="X(0)Y(1)Z(2)")
-    )
-    H = 1.0 * tq.paulis.X(0) + 2.0 * tq.paulis.Y(1) + 3.0 * tq.paulis.Z(2)
-    O = tq.ExpectationValue(U=U, H=H)
-
-    result = tq.optimizer_scipy.minimize(
-        objective=O, options={"maxfun": 2}, method="TNC", backend=simulator, samples=3, silent=True
-    )
-    assert len(result.history.energies) <= 3
-
-
 @pytest.mark.parametrize("simulator", simulators)
 def test_one_qubit_wfn(simulator):
     U = tq.gates.Trotterized(angles=["a"], steps=1, generators=[tq.paulis.Y(0)])
